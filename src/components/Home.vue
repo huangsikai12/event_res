3d3fff84b66bd5b7d2deac8a59ef0718ce146a71
huangsikai12 <huangsikai6666@gmail.com>
2024-09-15 20:39:20 +0800

    fix-image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@ import axios from "axios";
 import {BASE_URL} from "../Constants.ts";
 import {HomeGrid} from "../interface/DataInterface.ts";
 const homeGrid = ref<Array<HomeGrid>>([])
-const guideImage :Array<String>= [
+const guideImage :Array<string>= [
     "https://image.huangsikai.top/guide_1.jpg",
     "https://image.huangsikai.top/guide_2.jpg",
     "https://image.huangsikai.top/guide_3.jpg",
@@ -37,7 +37,7 @@ const gridClick=(value:HomeGrid)=>{
 
 <template>
   <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" style="margin-bottom: 30px">
-    <van-swipe-item v-for="image in guideImage" :key="image" >
+    <van-swipe-item v-for="image in guideImage">
       <van-image
         width="100%"
         height="80%"
