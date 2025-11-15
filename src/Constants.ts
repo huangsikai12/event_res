605564e943bc09b607fd1d8c6edf59e004c5c963
huangsikai <huangsikai6666@gmail.com>
2025-11-15 12:07:58 +0800

    更改URL

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "106.12.86.201:10001"
+export const BASE_URL = "http://106.12.86.201:10001"
 export const IMAGE_URL = "https://image.huangsikai.top"
 //export const BASE_URL = "http://localhost"
