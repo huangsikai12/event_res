8bcb3c69cabab8ffaaefe8a6bed32650b1aaf5a6
huangsikai <huangsikai6666@gmail.com>
2025-11-15 16:30:25 +0800

    更改URL

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://api.club.worldskills.top/"
+export const BASE_URL = "https://api.club.worldskills.top"
 export const IMAGE_URL = "https://image.huangsikai.top"
 //export const BASE_URL = "http://localhost"
